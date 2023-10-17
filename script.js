e7bfa0f7643c1a47d442a478ea0afc789800a27d
https-carlos <139911150+https-carlos@users.noreply.github.com>
2023-10-17 10:38:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ function adicionar(){
     cell3.innerHTML = data
 }
 
-function calcular(){
-    
+function calcular() {
+    let table = document.getElementById("tabela");
+    let total = 0;
+    let mes = {};
+    let categoria = {};
+
+    for (let i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+
+        let categoriaAtual = row.cells[0].textContent;
+        let valor = parseFloat(row.cells[1].textContent);
+        let data = new Date(row.cells[2].textContent);
+
+        total += valor;
+
+        let mesAno = data.toLocaleString('pt-BR', { month: '2-digit', year: 'numeric' });
+        mes[mesAno] = (mes[mesAno] || 0) + valor;
+
+        categoria[categoriaAtual] = (categoria[categoriaAtual] || 0) + valor;
+    }
+
+    let divCalculos = document.getElementById("calculos");
+    divCalculos.innerHTML = "Valor total: " + total.toFixed(2) + "<br><br>";
+
+    for (let m in mes) {
+        divCalculos.innerHTML += m + ": " + mes[m].toFixed(2) + "<br><br>";
+    }
+
+    for (let c in categoria) {
+        divCalculos.innerHTML += c + ": " + categoria[c].toFixed(2) + "<br><br>";
+    }
 }
